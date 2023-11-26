a9eb46fa3c87afcd3d583fe177d16057f10ec50a
Zafester <nickmanv2004@gmail.com>
2023-11-25 22:39:59 -0500

    segundo commit

--- src/main/java/pe/edu/cibertec/CL3_DSW_NicolasVillena/Cl3DswNicolasVillenaApplication.java
+++ src/main/java/pe/edu/cibertec/CL3_DSW_NicolasVillena/Cl3DswNicolasVillenaApplication.java
@@ -2,6 +2,9 @@ package pe.edu.cibertec.CL3_DSW_NicolasVillena;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Cl3DswNicolasVillenaApplication {
@@ -9,5 +12,15 @@ public class Cl3DswNicolasVillenaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Cl3DswNicolasVillenaApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer configCorsGlobal(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("api/v1/**")
+						.allowedMethods("GET", "POST", "PUT")
+						.allowedOrigins("https://www.cibertec.edu.pe");
+			}
+		};
+	}
 }
